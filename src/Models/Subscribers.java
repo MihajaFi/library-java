348692bbd8b469142b95c709bf0612ef70dccb3d
Fifaliana <hei.fifaliana@gmail.com>
2023-11-16 20:09:09 +0300

    feat: add a model for a subscribers

--- src/Models/Subscribers.java
+++ src/Models/Subscribers.java
@@ -1,2 +1,46 @@
-package Models;public class Subscribers {
+package Models;
+
+public class Subscribers {
+    private int id ;
+    private String name ;
+    private String reference ;
+
+    public Subscribers(int id, String name, String reference) {
+        this.id = id;
+        this.name = name;
+        this.reference = reference;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    @Override
+    public String toString() {
+        return "Subscribers{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", reference='" + reference + '\'' +
+                '}';
+    }
 }
