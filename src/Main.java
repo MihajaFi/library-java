c99197831613ae1321b76ec6f7d0cf4b9f0b32ff
Fifaliana <hei.fifaliana@gmail.com>
2023-11-16 08:45:04 +0300

    feat : remove test for connecting to database

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,8 @@ import java.sql.SQLException;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        test() ;
+
     }
-    public static Connection test(){
-        ConnectionConfig connectionConfig = new ConnectionConfig();
-        try(Connection connection = connectionConfig.createConnection()){
-            System.out.println("Connection ok");
-        } catch (SQLException e) {
-            throw new RuntimeException("Error ");
-        }
-        return null ;
-    }
+
+
 }
