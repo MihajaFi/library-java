6ea13f30a12cf32264b5892460be0d973fe4b87f
Fifaliana <hei.fifaliana@gmail.com>
2023-11-16 20:08:41 +0300

    feat: add a model for a author

--- src/Models/Author.java
+++ src/Models/Author.java
@@ -1,2 +1,46 @@
-package Models;public class Author {
+package Models;
+
+public class Author {
+    private  int id ;
+    private String AuthorName ;
+    private String sex ;
+
+    public Author(int id, String authorName, String sex) {
+        this.id = id;
+        AuthorName = authorName;
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", AuthorName='" + AuthorName + '\'' +
+                ", sex=" + sex +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthorName() {
+        return AuthorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        AuthorName = authorName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
