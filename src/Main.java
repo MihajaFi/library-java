29bd46127e814d35b39f6e615d2682ee77f7063d
Fifaliana <hei.fifaliana@gmail.com>
2023-11-17 23:49:05 +0300

    feat: add all test for all models

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-import Configuration.ConnectionConfig;
+import Models.Author;
+import Models.Book;
+import Models.topic_enum;
+import Repository.AuthorCrudOperations;
+import Repository.BookCrudOperations;
+import Test.AuthorCrudTest;
+import Test.BookCrudtTest;
+import Test.SubscribersCrudTest;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        AuthorCrudTest.AuthorTest();
+        SubscribersCrudTest.SubcribersTest();
+        BookCrudtTest.BookTest();
     }
 
 
